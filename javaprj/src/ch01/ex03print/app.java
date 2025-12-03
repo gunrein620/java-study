373c686843aa2751bad39dd62c7ca75b33b3b263
gunrein <gunrein620@gmail.com>
2025-12-03 11:18:48 +0900

    형식지정자의 이해

--- javaprj/src/ch01/ex03print/app.java
+++ javaprj/src/ch01/ex03print/app.java
@@ -6,7 +6,7 @@ import java.io.PrintStream;
 
 public class app {
   public static void main(String[] args) throws IOException {
-    int kor1 = 80;
+    int kor1 = 85;
     int kor2 = 90;
     int kor3 = 100;
 
@@ -26,13 +26,17 @@ public class app {
     out.close();
     fos.close();
 
-    System.out.print(kor1);
-    System.out.print(" ");
+    int total = kor1 + kor2 + kor3;
+    float avg = total / 3.0f; 
 
-    System.out.print(kor2);
-    System.out.print(" ");
+    System.out.println("--------------------------------");
+    System.out.println("|            성적표            |");
+    System.out.println("--------------------------------");
 
-    System.out.print(kor3);
-    System.out.print(" ");
+    System.out.printf("kor1 :%10d(%1$x)\n", kor1);
+    System.out.printf("kor2 :%d\n", kor2);
+    System.out.printf("kor3 :%-10d(%x)\n", kor3, kor3);
+    System.out.printf("total :%10d\n", total);
+    System.out.printf("avg :%6.2f\n", avg);
   }
 }
