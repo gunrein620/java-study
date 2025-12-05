3d82b97c2a2070243725f6f242f0c49d8cd7ec5e
gunrein <gunrein620@gmail.com>
2025-12-05 17:25:52 +0900

    콘솔 입력 및 코드지역화

--- javaprj/src/ch01/ex04input/app.java
+++ javaprj/src/ch01/ex04input/app.java
@@ -1,16 +1,53 @@
 package ch01.ex04input;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class app {
   public static void main(String[] args) throws IOException {
-    int kor1 = 85;
-    int kor2 = 90;
-    int kor3 = 100;
+    int kor1 = 0;
+    int kor2 = 0;
+    int kor3 = 0;
 
-    FileOutputStream fos = new FileOutputStream("/Users/kunwoopark/WS/java-study/javaprj/res/kors.txt"); 
+    //----------성적 로드 --------------
+    FileInputStream fis = new FileInputStream("/Users/kunwoopark/WS/java-study/javaprj/res/kors.txt");
+    Scanner scan = new Scanner(fis);
+
+    kor1 = scan.nextInt(); // 85
+    kor2 = scan.nextInt(); // 90
+    kor3 = scan.nextInt(); // 100
+    kor1++;
+
+    scan.close();
+    fis.close();
+
+    //----------성적 입력 --------------
+
+/*    int code = fis.read();
+
+    System.out.println(code); // 56 -> "56"
+    System.out.printf("%c\n",code);
+    System.out.write(code);
+    System.out.println();*/
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("kor1: ");
+    kor1 = scanner.nextInt();
+    System.out.print("kor2: ");
+    kor2 = scanner.nextInt();
+    System.out.print("kor3: ");
+    kor3 = scanner.nextInt();
+
+    scanner.close();
+
+    //----------성적 저장 --------------
+
+    System.out.println("=======================");
+
+    FileOutputStream fos = new FileOutputStream("/Users/kunwoopark/WS/java-study/javaprj/res/kors.txt");
     PrintStream out = new PrintStream(fos);
 
     out.print(kor1);
@@ -25,6 +62,8 @@ public class app {
 
     out.close();
     fos.close();
+
+    //----------성적 출력 --------------
 
     int total = kor1 + kor2 + kor3;
     float avg = total / 3.0f; 
---
+++ javaprj/src/ch01/ex05local/app.java
@@ -0,0 +1,76 @@
+package ch01.ex05local;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) throws IOException {
+        int kor1 = 0;
+        int kor2 = 0;
+        int kor3 = 0;
+
+        {   //----------성적 로드 --------------
+            FileInputStream fis = new FileInputStream("/Users/kunwoopark/WS/java-study/javaprj/res/kors.txt");
+            Scanner scan = new Scanner(fis);
+
+            kor1 = scan.nextInt(); // 85
+            kor2 = scan.nextInt(); // 90
+            kor3 = scan.nextInt(); // 100
+            kor1++;
+
+            scan.close();
+            fis.close();
+        }
+
+        //----------성적 입력 --------------
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("kor1: ");
+            kor1 = scanner.nextInt();
+            System.out.print("kor2: ");
+            kor2 = scanner.nextInt();
+            System.out.print("kor3: ");
+            kor3 = scanner.nextInt();
+
+            scanner.close();
+        }
+        //----------성적 저장 --------------
+        {
+            System.out.println("=======================");
+
+            FileOutputStream fos = new FileOutputStream("/Users/kunwoopark/WS/java-study/javaprj/res/kors.txt");
+            PrintStream out = new PrintStream(fos);
+
+            out.print(kor1);
+            out.print(" ");
+
+            out.print(kor2);
+            out.print(" ");
+
+            out.print(kor3);
+            out.print(" ");
+
+
+            out.close();
+            fos.close();
+        }
+        //----------성적 출력 --------------
+        {
+            int total = kor1 + kor2 + kor3;
+            float avg = total / 3.0f;
+
+            System.out.println("--------------------------------");
+            System.out.println("|            성적표            |");
+            System.out.println("--------------------------------");
+
+            System.out.printf("kor1 :%10d(%1$x)\n", kor1);
+            System.out.printf("kor2 :%d\n", kor2);
+            System.out.printf("kor3 :%-10d(%x)\n", kor3, kor3);
+            System.out.printf("total :%10d\n", total);
+            System.out.printf("avg :%6.2f\n", avg);
+        }
+    }
+}
